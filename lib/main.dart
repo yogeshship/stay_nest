63ead07872b17b576abd25bb3100947c5852a8fa
Yogesh Sah <sahyogesh807@gmail.com>
2026-08-14 14:16:46 +0545

    Configure Firebase core for StayNest

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
 import 'screens/welcome_screen.dart';
 import 'theme/app_theme.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const StayNestApp());
 }
 
